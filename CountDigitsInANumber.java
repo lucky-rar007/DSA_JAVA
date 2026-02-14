c90c83fd45f5422e656eb548243ddb8beebc950c
lucky-rar007 <lakshrajsc1234@gmail.com>
2026-02-14 09:30:55 +0530

    count digits in a number question solved

---
+++ CountDigitsInANumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountDigitsInANumber {
+    public static void printcount(int n) {
+        if (n == 0) {
+            System.out.println("1");
+        } else {
+            n = Math.abs(n);
+            int count = 0;
+            while (n > 0) {
+                n = n / 10;
+                count++;
+            }
+            System.out.println(count);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        sc.close();
+        printcount(n);
+    }
+
+}
