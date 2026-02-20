97eb9f67bfc72c0578dbf6801c4ec4d30b310591
lucky-rar007 <lakshrajsc1234@gmail.com>
2026-02-20 10:56:26 +0530

    Print all divisors problem solved

---
+++ PrintAllDivisors.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PrintAllDivisors {
+    public static void print(int N) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+        int sqrtN = (int) Math.sqrt(N);
+        for (int i = 1; i <= sqrtN; i++) {
+            if (N % i == 0) {
+                divisors.add(i); 
+                if (i != N / i)
+                    divisors.add(N / i); 
+            }
+        }
+        Collections.sort(divisors);
+        System.out.println(divisors);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        print(N);
+    }
+}
